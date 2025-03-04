24dc86c7c4a1c8a0db68576db20cec6bc6ec186f
mali0527 <al27m7911@gmail.com>
2025-03-04 12:10:26 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,20 @@
 // Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase Configuration
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCQZ9RaBLOwdlQw4qTICKSrb5oi2Sdgx_4",
+    authDomain: "cadetwelfareform.firebaseapp.com",
+    projectId: "cadetwelfareform",
+    storageBucket: "cadetwelfareform.firebasestorage.app",
+    messagingSenderId: "732795642984",
+    appId: "1:732795642984:web:ca6fa03208e6ced26907dd",
+    measurementId: "G-NZJ3NLM21F"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
