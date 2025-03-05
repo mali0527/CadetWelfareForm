68e4f49503116adf4347c80f056bffe01be9501c
mali0527 <al27m7911@gmail.com>
2025-03-05 12:01:15 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,13 @@
 // Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
 
 // Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCQZ9RaBLOwdlQw4qTICKSrb5oi2Sdgx_4",
   authDomain: "cadetwelfareform.firebaseapp.com",
   projectId: "cadetwelfareform",
-  storageBucket: "cadetwelfareform.appspot.com",  // Fixed storage bucket
+  storageBucket: "cadetwelfareform.appspot.com",
   messagingSenderId: "732795642984",
   appId: "1:732795642984:web:ca6fa03208e6ced26907dd",
   measurementId: "G-NZJ3NLM21F"
@@ -17,7 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
 // Export Firestore for use in script.js
 export { db, collection, addDoc };
